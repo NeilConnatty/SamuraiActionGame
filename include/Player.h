e13957ac3bc724aa1c96f08ac1fc238a024b5d95
Neil Connatty <neil.connatty@gmail.com>
2026-02-04 20:47:28 -0800

    use AABB intersections to resolve player collisions

--- include/Player.h
+++ include/Player.h
@@ -18,6 +18,6 @@ private:
     std::array<bool, 4> m_inputs;
     sf::Texture m_texture{"../../assets/sprites/character.png"};
     sf::Sprite m_sprite{m_texture, {{0,0}, {16, 32}}};
-    sf::FloatRect m_boundingBox{{0.f, 16.f}, {16.f, 16.f}};
+    sf::FloatRect m_collider{{0.f, 16.f}, {16.f, 16.f}};
     const Map& m_map;
 };
